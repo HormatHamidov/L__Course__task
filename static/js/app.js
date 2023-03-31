aba687f9afa1226cef8842bfce309df4f3acfbe8
Hormat Hamidov <hamidovhormat@coders.edu.az>
2023-04-01 01:45:02 +0400

    boots

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+var a = 5
+var b = 5
+
+if (a > b) {
+    console.log("A ededi B den boyukdur")
+} else if (a < b) {
+    console.log("B ededi A dan boyukdur")
+} else {
+    console.log("Bu ededler beraberdir")
+}
